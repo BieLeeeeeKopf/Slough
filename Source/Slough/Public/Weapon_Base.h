d927174639aad49c1937edf1826a6fcd706f99bd
BieLeeeeeKopf <1289452313@qq.com>
2025-06-06 19:33:18 +0800

    添加Weapon_Base类

---
+++ Source/Slough/Public/Weapon_Base.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Weapon_Base.generated.h"
+
+UCLASS()
+class SLOUGH_API AWeapon_Base : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AWeapon_Base();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	int32 maxAmmo;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	int32 currentAmmo;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	int32 maxMagazineAmmunition;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	int32 currentMagazineAmmunition;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
+	FName weaponSlot;
+};
